81ff3e51b739b19f34b576b449117b8e83dad0da
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-03 22:00:14 +0900

    🧪 : TS 실험 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Greetings from "./Greetings";
 
-function App() {
-  return (
-    <div>
-      <h1>테스트</h1>
-      <h1>{process.env.REACT_APP_HOST}</h1>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} `);
+  };
+
+  return <Greetings name="으아" onClick={onClick} optional="옵션" />;
+};
 
 export default App;
+
+// const App: React.FC = () => {
+//   const onClick = (name: string) => {
+//     console.log(`${name} says hello`);
+//   };
+
+//   return <Greetings name="Hello" onClick={onClick} />;
+// };
