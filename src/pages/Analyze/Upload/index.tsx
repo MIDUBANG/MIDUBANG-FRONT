919bffbc57b6cc877bc0924c3f95b1bcfed51901
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-08 20:25:46 +0900

    🚀 : craco 적용

---
+++ src/pages/Analyze/Upload/index.tsx
@@ -0,0 +1,12 @@
+import SimpleNavBar from "@components/NavBar/SimpleNavBar";
+
+const Upload = () => {
+  return (
+    <div>
+      <SimpleNavBar />
+      업로드 페이지
+    </div>
+  );
+};
+
+export default Upload;
