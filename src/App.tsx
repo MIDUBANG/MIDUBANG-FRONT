9e6a2c1acc17e64ae7910dff5984b9355d8c5d5e
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-10 18:56:31 +0900

    🚀 : swiper.js 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import loadable from "@loadable/component";
 
-//import Test from "./Test";
-//import Home from "./pages/Analyze/Home";
-//import InputTest from "./InputTest";
+
 
 const Upload = loadable(() => import("@pages/Analyze/Upload"));
 
@@ -21,8 +19,16 @@ const App: React.FC = () => {
 };
 
 export default App;
-{
+
+
+
+
+  //import Test from "./Test";
+//import Home from "./pages/Analyze/Home";
+//import InputTest from "./InputTest";
+
+
   /* <InputTest />
       <Home name="props" onClick={TestOnClick} />
       <Test /> */
-}
+
--- src/pages/Analyze/Upload/index.tsx
+++ src/pages/Analyze/Upload/index.tsx
@@ -1,12 +1,51 @@
+import styled from "@emotion/styled";
 import SimpleNavBar from "@components/NavBar/SimpleNavBar";
+import logoPerson from "@assets/illustration/logo&person.png";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Autoplay } from "swiper";
+import "swiper/swiper.min.css";
+
+import "./style.css";
+import "swiper/components/pagination/pagination.scss";
 
 const Upload = () => {
+  SwiperCore.use([Pagination, Autoplay]);
+
   return (
-    <div>
+    <Div>
       <SimpleNavBar />
-      업로드 페이지
-    </div>
+
+      <Swiper pagination={{ clickable: true }} className="mySwiper">
+        <SwiperSlide>
+          <Illustration src={logoPerson} />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </Div>
   );
 };
 
 export default Upload;
+
+const Div = styled.div`
+  width: 100%;
+`;
+
+const CardSectionDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 30px;
+  border: 1px solid gray;
+`;
+
+const Illustration = styled.img`
+  margin: 50px auto 0 auto;
+`;
