6b25f8410941ce0149954d861eda11f0674fdb93
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-13 22:03:14 +0900

    🚀 : 네브바 서브text props 추가

--- src/components/NavBar/SimpleNavBar.tsx
+++ src/components/NavBar/SimpleNavBar.tsx
@@ -1,15 +1,21 @@
 import styled from "@emotion/styled";
 
-import MiniLogo from "../../assets/logo/MiniLogo.svg";
-import CancleLogo from "../../assets/icon/Cancle.png";
-
+import MiniLogo from "@assets/logo/MiniLogo.svg";
+import CancleLogo from "@assets/icon/Cancle.png";
+import rod from "@assets/icon/rod.svg";
 import "@style/font.css";
 
-const SimpleNavBar = () => {
+type SimpleNavBarProps = {
+  text?: string;
+};
+
+const SimpleNavBar = ({ text }: SimpleNavBarProps) => {
   return (
     <BarDiv>
       <img src={MiniLogo} className="mini-logo" />
       <p>믿어방</p>
+      {text !== "" && <Rod src={rod} />}
+      <p>{text}</p>
       <img src={CancleLogo} className="cancle-logo" />
     </BarDiv>
   );
@@ -17,6 +23,13 @@ const SimpleNavBar = () => {
 
 export default SimpleNavBar;
 
+SimpleNavBar.defaultProps = {
+  text: "",
+};
+
+const Rod = styled.img`
+  margin-left: 12px;
+`;
 const BarDiv = styled.div`
   display: flex;
   width: 100%;
