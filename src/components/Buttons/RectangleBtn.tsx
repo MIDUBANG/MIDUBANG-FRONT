e875dbd718769217c69e6beb2cfe99a67847c026
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-14 19:23:41 +0900

    🚀 : 짧은 버튼

---
+++ src/components/Buttons/RectangleBtn.tsx
@@ -0,0 +1,49 @@
+import styled from "@emotion/styled";
+import cloud from "@assets/icon/cloud.png";
+
+type Props = {
+  text: string;
+  color: string;
+  activeColor: string;
+};
+
+const RectangleBtn = ({ text, color, activeColor }: Props) => {
+  console.log(activeColor);
+
+  return (
+    <Btn color={color} activeColor={activeColor}>
+      {text}
+    </Btn>
+  );
+};
+
+export default RectangleBtn;
+
+RectangleBtn.defaultProps = {
+  activeColor: "--aurora-shadow",
+};
+
+const Btn = styled.div<{ activeColor: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 119px;
+  height: 41px;
+  border-radius: 8px;
+
+  padding: 0 21px;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+
+  // 색상
+  background: var(${(props) => props.color});
+  box-shadow: -3px 4px 5px rgba(0, 0, 0, 0.15);
+  &:active {
+    background: var(${(props) => props.activeColor});
+  }
+`;
