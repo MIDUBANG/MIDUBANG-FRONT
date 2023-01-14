953a32b9fc94758eea8401500bb7f1cc43e17f91
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-14 19:47:33 +0900

    🚀 : LongBtn

---
+++ src/components/Buttons/LongBtn.tsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+
+type Props = {
+  text: string;
+  color: string;
+  activeColor: string;
+};
+
+const LongBtn = ({ text, color, activeColor }: Props) => {
+  return (
+    <Btn color={color} activeColor={activeColor}>
+      {text}
+    </Btn>
+  );
+};
+
+export default LongBtn;
+
+LongBtn.defaultProps = {
+  activeColor: "--aurora-shadow",
+};
+
+const Btn = styled.div<{ activeColor: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 294px;
+  height: 41px;
+
+  border-radius: 8px;
+
+  padding: 0 21px;
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+
+  // 색상
+  background: var(${(props) => props.color});
+  box-shadow: -3px 4px 5px rgba(0, 0, 0, 0.15);
+  &:active {
+    background: var(${(props) => props.activeColor});
+  }
+`;
