55b137287755b9403d228e3ee7ff2986cefe7f10
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-01-28 22:23:31 +0900

    🚀 : http component

---
+++ src/api/http.tsx
@@ -0,0 +1,17 @@
+// import axios from "axios";
+
+// export const http = axios.create({
+//   baseURL: "https://api.rewha2022.com",
+// });
+
+// http.defaults.withCredentials = true;
+
+// const token = JSON.parse(localStorage.getItem("token")) ?? false;
+
+// http.defaults.headers.common["Authorization"] = token
+//   ? `Bearer ${token}`
+//   : null;
+
+export const Http = () => {
+  return <p>dd</p>;
+};
