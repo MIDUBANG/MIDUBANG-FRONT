e4faa19bd6c629767084b8551687e353d4ec89e9
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-02-11 16:26:58 +0900

    🚀 : Word page 작업

--- src/pages/User/Word/WordList.tsx
+++ src/pages/User/Word/WordList.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
 import { GetWordList, PostSaveWord, DeleteWord } from "@api/word";
 import { useCookies } from "react-cookie";
 
 // componnet
 import SimpleNavBar from "@components/NavBar/SimpleNavBar";
-import ShortBtn from "@components/Buttons/ShortBtn";
+
+// image
+import temp from "@assets/illustration/logo&person.png";
+import calendar from "@assets/wordlist/calendar.png";
+import bookmark from "@assets/wordlist/bookmark.svg";
+
 const WordList = () => {
   const [cookies, setCookie, removeCookie] = useCookies(["refreshToken"]);
+
+  console.log("리프레시는", cookies.refreshToken);
 
   const onCookie = (res: any) => {
     console.log("쿠키");
@@ -17,22 +25,15 @@ const WordList = () => {
     setCookie("refreshToken", refreshToken, { path: "/" });
   };
 
-  const [wordId, setWordId] = useState(1);
+  const [wordId, setWordId] = useState(2);
 
   const [wordList, setWordList] = useState<
-    { word_id: number; word: string; meaning: string; date: string }[]
+    { word_id: number; word: string; meaning: string; word_date: string }[]
   >([]);
 
   const [activeWord, setActiveWord] = useState<
-    { word_id: number; word: string; meaning: string; date: string }[]
-  >([
-    {
-      word_id: 1,
-      word: "근저당",
-      meaning: "근저당 설명 블라블라",
-      date: "2022-01-21",
-    },
-  ]);
+    { word_id: number; word: string; meaning: string; word_date: string }[]
+  >([]);
 
   // 선택한 날짜
   const [date, setDate] = useState("");
@@ -44,12 +45,17 @@ const WordList = () => {
 
   const _handleWordList = async () => {
     const words = await GetWordList(cookies.refreshToken, onCookie);
-    setWordList(words);
+    const newWords = words.map((w: any) => {
+      let date = w.word_date.replaceAll("-", ".");
+      return { ...w, word_date: date };
+    });
+    setWordList(newWords);
+    console.log(newWords);
   };
 
-  const _handleDeleteWord = async () => {
+  const _handleDeleteWord = async (wordId: number) => {
+    alert("삭제하시겠습니까?");
     await DeleteWord(wordId, cookies.refreshToken, onCookie);
-    // setWordList([]);
     _handleWordList();
   };
 
@@ -58,37 +64,57 @@ const WordList = () => {
     // 날짜 정하면, 그거랑 맞는 wordList만 출력
   };
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    _handleWordList(); // 가져오기
+  }, []);
+
   return (
     <Div>
       <SimpleNavBar text="단어장" />
 
-      <div>달력 아이콘</div>
+      <Container>
+        <Illustration src={temp} />
+        <Calendar src={calendar} />
 
-      <div>
-        <button onClick={() => _handleWordList()}>전체</button>
-        <button onClick={() => _handleSaveWord(wordId)}>오늘(저장)</button>
-      </div>
+        <BtnBox>
+          <Button active={true}>전체</Button>
+          <Button active={false}>오늘</Button>
+        </BtnBox>
 
-      {activeWord.map((word) => (
-        <div key={word.word_id}>
-          <div>
-            <p>{word.word}</p>
-            <p>{word.date}</p>
-          </div>
+        {wordList.map((word) => (
+          <Word
+            key={word.word_id}
+            onClick={() => navigate(`/wordmean/${word.word_id}`)}
+          >
+            <div className="title">
+              <p className="word"> {word.word}</p>
+              <p className="date">{word.word_date}</p>
+            </div>
 
-          <p>{word.meaning}</p>
-          <div>북마크로고</div>
-        </div>
-      ))}
+            <p className="des">{word.meaning}</p>
 
-      {/* <button onClick={() => setWordId(wordId + 1)}>{wordId}</button>
-      <button onClick={() => _handleDeleteWord()}>저장한 단어 삭제</button> */}
+            <img
+              src={bookmark}
+              className="bookmark"
+              onClick={(e) => {
+                e.stopPropagation();
+                _handleDeleteWord(word.word_id);
+              }}
+            />
+          </Word>
+        ))}
 
-      <WordListBox>
-        {wordList.map((word) => {
-          return <p key={word.word_id}>{word.word}</p>;
-        })}
-      </WordListBox>
+        <button onClick={() => setWordId(wordId + 1)}>{wordId}</button>
+        <button onClick={() => _handleSaveWord(wordId)}>저장</button>
+
+        {/* <WordListBox>
+  {wordList.map((word) => {
+    return <p key={word.word_id}>{word.word}</p>;
+  })}
+</WordListBox> */}
+      </Container>
     </Div>
   );
 };
@@ -99,6 +125,96 @@ const Div = styled.div`
   height: 100%;
 `;
 
-const WordListBox = styled.div`
-  border: 1px red solid;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Illustration = styled.img`
+  width: 179px;
+  height: 115px;
+  margin: 15px auto 0 auto;
+`;
+
+const Calendar = styled.img`
+  width: 39px;
+  height: 39px;
+  margin-left: auto;
+  margin-right: 22px;
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 23px;
+  margin-top: 19px;
+  margin-bottom: 37px;
+`;
+const Button = styled.div<{ active: boolean }>`
+  width: 50%;
+  height: 45px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 25px;
+
+  border: 1px solid ${(props) => (props.active ? "#1F4EF5" : "#9a9a9a")};
+  color: ${(props) => (props.active ? "#1F4EF5" : "#9a9a9a")};
+
+  margin-left: 7px;
+  margin-right: 7px;
+`;
+
+const Word = styled.div`
+  width: 331px;
+  height: auto;
+  background: #f4f5f7;
+  border-radius: 5px;
+
+  margin: 16px auto 0 auto;
+  padding: 20px;
+
+  .title {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .word {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 25px;
+  }
+
+  .date {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 350;
+    font-size: 14px;
+    line-height: 20px;
+
+    color: rgba(0, 0, 0, 0.5);
+  }
+
+  .des {
+    margin-top: 10px;
+    margin-bottom: 15px;
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 20px;
+  }
+
+  .bookmark {
+    z-index: 1000;
+  }
 `;
