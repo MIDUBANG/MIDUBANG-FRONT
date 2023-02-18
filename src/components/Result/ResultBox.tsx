eb32e307cc5013c7870fff218b223f30cde6846a
dy6578ekdbs <dy6578ekdbs@gmail.com>
2023-02-18 16:10:32 +0900

    🚀 : result page 퍼블리싱

--- src/components/Result/ResultBox.tsx
+++ src/components/Result/ResultBox.tsx
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 import ill from "@assets/illustration/loadingPerson.png";
 import alert from "@assets/icon/alert.svg";
 
+import arrow from "@assets/icon/rightarrow.svg";
+
 const ResultBox = () => {
   const description =
     "주택임대차보호법에서 보호하는 임차인의 거주 기간은 2년입니다.@주택임대차보호법 제 4조에 따르면 기간을 정하지 않거나 2년 미만으로 정한 임대차 계약 기간은 그 기간을  2년으로 봅니다.";
@@ -10,20 +12,40 @@ const ResultBox = () => {
   let desArray = description.split("@");
   return (
     <Block>
+      <Contract>
+        <div></div>
+        <p> 보증금과 월세는 1년마다 시세에 맞게 올릴 수 있다?</p>
+      </Contract>
+
       <img src={ill} />
+      <Describe>
+        <p>
+          특약에 관련 내용을 넣었고, 주변 전·월세 시세가 단기간에 아무리
+          급등했어도 집주인이 맘대로 올릴 수 없습니다. 주택임대차보호법에서
+          임대료는 2년마다 연 5% 한도로 증액할 수 있다고 규정하고 있습니다.
+          그것도 임차인이 동의할 경우에만 가능하다는 것이 국토부의 유권
+          해석입니다.
+        </p>
+        <p>
+          임차인이 5% 증액에 동의하지 않을 경우, 집주인은 임차주택에 대한
+          조세·공과금 증감, 경제사정 변동 등 임대료를 올려야 하는 이유를
+          증명해야 증액 청구가 가능합니다. 따라서 특약으로 이러한 조항을 넣어도
+          효력이 없고, 임대차 보호법에 나온 한도 내에서 증액할 수 있습니다.
+        </p>
+        <p>
+          전월세계약이 만기시에, 임차인은 만기 2개월전까지 임대인에게
+          갱신청구권을 행사할 수 있습니다. 이때는 임대인은 이를 거절할 수 없고,
+          전임대차와 같은 조건으로 2년간 재계약 연장됩니다. 다만, 임대인은
+          전임대차의 차임의 5%이내에서만 증액을 요청할 수 있습니다. 보증금과
+          월차임 각각에 대하여 5%증액이 그 한도액이 됩니다.
+        </p>
+      </Describe>
 
-      <Title>
-        <img src={alert} />
-        <p>"세입자는 1년 뒤 퇴거한다."</p>
-      </Title>
+      <NewsBtn>
+        <p>기사보기</p>
 
-      <div>
-        {desArray.map((d) => (
-          <p>{d}</p>
-        ))}
-      </div>
-
-      <NewsBtn>기사 보기</NewsBtn>
+        <img src={arrow} />
+      </NewsBtn>
     </Block>
   );
 };
@@ -31,23 +53,76 @@ const ResultBox = () => {
 export default ResultBox;
 
 const Block = styled.div`
-  margin: 0 auto;
-  width: 90%;
-  height: auto;
-  border: 0.5px solid rgba(147, 147, 147, 0.5);
-  border-radius: 10px;
-
   box-sizing: border-box;
-  padding: 0 23px;
+  height: auto;
+
+  margin: 18px;
+
+  border: 0.5px solid rgba(147, 147, 147, 0.5);
+  border-radius: 5px;
 `;
 
-const Title = styled.div`
+const Contract = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+
+  margin: 26px auto 30px 24px;
+
+  width: 70%;
+
+  div {
+    background: #fbb03b;
+    height: auto;
+    width: 7px;
+    margin-right: 10px;
+  }
+  p {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 23px;
+  }
+`;
+
+const Describe = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 28px;
+  p {
+    margin-bottom: 20px;
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 15px;
+    line-height: 20px;
+  }
 `;
 
 const NewsBtn = styled.div`
-  margin-top: 100px;
-  margin-bottom: 40px;
+  width: 124px;
+  height: 34px;
+
+  border: 1px solid #4880ee;
+  border-radius: 20.5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 16px 20px auto;
+
+  p {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 19px;
+    text-align: center;
+    color: #4880ee;
+  }
+
+  img {
+    margin-left: 5px;
+    width: 14px;
+    height: 10px;
+  }
 `;
--- src/pages/Analyze/Result.tsx
+++ src/pages/Analyze/Result.tsx
@@ -2,24 +2,22 @@
 import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 
-import SimpleNavBar from "@components/NavBar/SimpleNavBar";
-
-import { FontTitle, FontDescribed } from "@style/font.style";
-import resultTemp1 from "@assets/temp/resultTemp1.png";
-import alert from "@assets/icon/alert.svg";
-
 import { useCookies } from "react-cookie";
 
 // component
+import { FontTitle, FontDescribed } from "@style/font.style";
+
+import SimpleNavBar from "@components/NavBar/SimpleNavBar";
+
 import ResultBox from "@components/Result/ResultBox";
 import WordModal from "@components/Modal/WordModal";
-
 //api
 import { PostAnalyze } from "@api/analyze";
 import { RootState } from "@store/store";
 import { useAppSelector } from "@store/store";
 // asset
 import { resultsType, CasesType, WordsType } from "@assets/types";
+import temp from "@assets/result/temp.png";
 
 const Result = () => {
   const requestData = useAppSelector((state: RootState) => state.result);
@@ -80,40 +78,33 @@ const Result = () => {
     <Div>
       <SimpleNavBar text="레포트" />
       <Container>
-        <FontTitle margin="20px 0 28px 28px">
-          주의 요망 특약 사항 발견
-        </FontTitle>
-        <img
-          src={resultTemp1}
-          style={{ margin: "0 auto", display: "block", width: "90%" }}
-        />
+        <Title>
+          <div></div>
+          <p> 주의 요망 특약 사항 발견</p>
+        </Title>
 
-        <p>특약 조항 해설</p>
+        <InfoBox>
+          <div className="text-info">
+            <p>#태그</p>
+            <p>#태그</p>
 
-        <ResultBox />
-        <ResultBox />
+            <p>2023.02.18 분석</p>
+          </div>
+          <div className="word-toggle">
+            <input type="checkbox" id="toggle" hidden />
+
+            <label htmlFor="toggle" className="toggleSwitch">
+              <p className="word">단어</p>
+              <span className="toggleButton"></span>
+            </label>
+          </div>
+        </InfoBox>
+
+        <img src={temp} />
 
         <ResultBox />
 
         <WordModal open={modalOpen} close={closeModal} text={selectWord} />
-
-        {/* <BtnsDiv>
-          <ShortBtn
-            text="관련 기사 보러가기"
-            color="--aurora"
-            activeColor="--aurora-shadow"
-            state={modalOpen}
-            onClick={openModal}
-          />
-          <div className="gap"></div>
-          <ShortBtn
-            text="단어 사전 보기"
-            color="--skyblue"
-            activeColor="--skyblue-shadow"
-            state={wordMark}
-            onClick={setWordMark}
-          />
-        </BtnsDiv> */}
       </Container>
     </Div>
   );
@@ -130,4 +121,109 @@ const Div = styled.div`
 
 const Container = styled.div`
   padding-bottom: 100px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  margin-left: 35px;
+  margin-bottom: 14px;
+
+  div {
+    background: #fbb03b;
+    height: auto;
+    width: 7px;
+    margin-right: 10px;
+  }
+  p {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 29px;
+  }
+`;
+
+const InfoBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 35px 20px 35px;
+
+  .text-info {
+    display: flex;
+
+    justify-content: space-between;
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    letter-spacing: -0.05em;
+    color: #9a9a9a;
+
+    p {
+      margin-right: 6px;
+    }
+  }
+
+  .word-toggle {
+    .toggleSwitch {
+      width: 52px;
+      height: 23px;
+
+      display: block;
+      position: relative;
+      border-radius: 30px;
+      background-color: #d9d9d9;
+      cursor: pointer;
+    }
+
+    .toggleSwitch .toggleButton {
+      width: 20px;
+      height: 20px;
+
+      position: absolute;
+      top: 50%;
+      left: 3px;
+
+      transform: translateY(-50%);
+      border-radius: 50%;
+      background: white;
+    }
+    .word {
+      display: none;
+    }
+
+    #toggle:checked ~ .toggleSwitch {
+      //체크 된 경우
+      background: #83b4f9;
+    }
+
+    #toggle:checked ~ .toggleSwitch .word {
+      position: absolute;
+      top: 4px;
+      left: 8px;
+      display: flex;
+      font-family: "Noto Sans KR";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 10px;
+      line-height: 14px;
+      text-align: center;
+      letter-spacing: -0.05em;
+
+      color: #ffffff;
+    }
+
+    #toggle:checked ~ .toggleSwitch .toggleButton {
+      left: calc(100% - 23px);
+      background: white;
+    }
+
+    .toggleSwitch,
+    .toggleButton {
+      transition: all 0.2s ease-in;
+    }
+  }
 `;
