5b97d2711b99f251ae33b9a2374ce1e8cb226b8b
dy657 <dy6578ekdbs@gmail.com>
2023-03-28 23:29:13 +0900

    ✨ 메인 페이지 생성

--- src/pages/Main/Home.tsx
+++ src/pages/Main/Home.tsx
@@ -1,14 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import MainNavBar from "@components/NavBar/MainNavBar";
+import profile from "@assets/nav/profile.png";
+
 const Home = () => {
   const navigate = useNavigate();
   const _ClickGotoLogin = () => {
     navigate("/login");
   };
   return (
-    <p>
-      메인 페이지입니다. <button onClick={_ClickGotoLogin}>로그인하기</button>
-    </p>
+    <Div>
+      <MainNavBar text="자취가 처음이라면" />
+      <div>배너</div>
+      <div>
+        <h1>제목1</h1>
+        <div>버튼들</div>
+      </div>
+      <div>
+        <h1>제목2</h1>
+        <div>버튼들</div>
+      </div>
+      <div>sdfs</div>
+    </Div>
   );
 };
 
 export default Home;
+
+const Div = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const TopNav = styled.div`
+  width: 100%;
+  height: 50px;
+`;
