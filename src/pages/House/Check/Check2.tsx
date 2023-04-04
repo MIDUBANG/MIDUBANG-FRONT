7f7b4dfd3058e84e71fd96d0530775ee128549d5
dy657 <dy6578ekdbs@gmail.com>
2023-04-05 00:46:51 +0900

    ⭐나머지 check page 파일만 생성

---
+++ src/pages/House/Check/Check2.tsx
@@ -0,0 +1,102 @@
+// lib
+import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import "react-circular-progressbar/dist/styles.css";
+//component
+import UnderstandBtn from "@components/Buttons/UnderstandBtn";
+import SimpleNavBar from "@components/NavBar/SimpleNavBar";
+import CustomAccordion from "@components/House/CustomAccordion";
+// asset
+import temp1 from "@assets/house/temp1.png";
+
+const Check2 = () => {
+  const check1Data = [
+    {
+      id: "0",
+      title: "내가 계약하려는 집과 주소가 동일한가요?",
+      content:
+        "계약하려는 부동산의 주소와 등기부등본에 기재되어 있는 소재지가 일치되어 있는지 확인하세요!",
+    },
+    {
+      id: "1",
+      title: "내가 계약하려는 집과 주소가 동일한가요?",
+      content:
+        "계약하려는 부동산의 주소와 등기부등본에 기재되어 있는 소재지가 일치되어 있는지 확인하세요!",
+    },
+    {
+      id: "2",
+      title: "내가 계약하려는 집과 주소가 동일한가요?",
+      content:
+        "계약하려는 부동산의 주소와 등기부등본에 기재되어 있는 소재지가 일치되어 있는지 확인하세요!",
+    },
+  ];
+
+  return (
+    <Div>
+      <SimpleNavBar text="등기부등본 마스터" direction="up" />
+
+      <Container>
+        <Title>갑구 - CHECKPOINT</Title>
+        <DesText>갑구에서 확인해야 할 부분들입니다.</DesText>
+
+        <img src={temp1} />
+
+        <CustomAccordion inputData={check1Data} />
+        <UnderstandBtn id={4} subId={2} />
+      </Container>
+    </Div>
+  );
+};
+
+export default Check2;
+
+const Div = styled.div`
+  width: 100%;
+  height: 100%;
+
+  img {
+    margin-top: 10px;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+  padding-top: 65px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.p`
+  margin: 23px auto 0 0;
+  font-family: "Noto Sans KR", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 26px;
+  line-height: 38px;
+  padding: 0;
+`;
+
+const ExImgText = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 350;
+  font-size: 14px;
+  line-height: 20px;
+  color: #3d3d3d;
+`;
+
+const DesText = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 350;
+  font-size: 14px;
+  line-height: 20px;
+  color: #000000;
+
+  span {
+    font-weight: 500;
+  }
+`;
--- src/route/CheckRoutes.tsx
+++ src/route/CheckRoutes.tsx
@@ -2,15 +2,14 @@ import React, { useState, useCallback } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Check1 from "@pages/House/Check/Check1";
-import Receive2 from "@pages/House/Receive/Receive2";
-import Receive3 from "@pages/House/Receive/Receive3";
-
+import Check2 from "@pages/House/Check/Check2";
+import Check3 from "@pages/House/Check/Check";
 const CheckRoutes = () => {
   return (
     <Routes>
       <Route element={<Check1 />} path="/1" />
-      <Route element={<Receive2 />} path="/2" />
-      <Route element={<Receive3 />} path="/3" />
+      <Route element={<Check2 />} path="/2" />
+      <Route element={<Check3 />} path="/3" />
     </Routes>
   );
 };
