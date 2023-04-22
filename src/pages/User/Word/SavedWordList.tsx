b478696ebadbc482a5938e609adf5c75c1790caf
dy657 <dy6578ekdbs@gmail.com>
2023-04-22 23:45:33 +0900

    🐛 저장된 단어 페이지

--- src/pages/User/Word/SavedWordList.tsx
+++ src/pages/User/Word/SavedWordList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 import { GetSavedWordList } from "@api/word";
 import { useCookies } from "react-cookie";
 
@@ -8,10 +9,8 @@ import { useCookies } from "react-cookie";
 import SimpleNavBar from "@components/NavBar/SimpleNavBar";
 
 // image
-import temp from "@assets/illustration/logo&person.png";
-import calendar from "@assets/wordlist/calendar.png";
-import bookmark from "@assets/wordlist/bookmark.svg";
-
+import wordbook from "@assets/wordlist/wordbook.png";
+import bookmark from "@assets/wordlist/bookmark.png";
 const SavedWordList = () => {
   const [cookies, setCookie, removeCookie] = useCookies(["refreshToken"]);
 
@@ -26,7 +25,7 @@ const SavedWordList = () => {
   };
 
   const [savedWordList, setSavedWordList] = useState<
-    { word_id: number; word: string; meaning: string; word_date: string }[]
+    { wordId: number; word: string; meaning: string; word_date: string }[]
   >([]);
 
   const navigate = useNavigate();
@@ -48,23 +47,82 @@ const SavedWordList = () => {
     _handleGetAllWordList(); // 저장한 단어 가져오기
   }, []);
 
-  const style = { marginBottom: "10px" };
+  const [btnArr, setBtnArr] = useState([
+    { id: 1, text: "최신순", active: true },
+    { id: 2, text: "과거순", active: false },
+  ]);
+
+  const _handleClickBtn = (id: number) => {
+    setBtnArr(
+      btnArr.map((b) =>
+        b.id === id ? { ...b, active: true } : { ...b, active: false }
+      )
+    );
+  };
+
+  /** 정렬 */
+  useEffect(() => {
+    console.log("정렬결과");
+
+    var newArray = [...savedWordList];
+
+    if (btnArr[0].active) {
+      newArray.sort((a, b) => {
+        // 최신순
+        if (a.word_date > b.word_date) return -1;
+        if (a.word_date < b.word_date) return 1;
+        return 0;
+      });
+    } else {
+      newArray.sort((a, b) => {
+        // 과거순
+        if (a.word_date > b.word_date) return 1;
+        if (a.word_date < b.word_date) return -1;
+        return 0;
+      });
+    }
+
+    setSavedWordList(newArray);
+  }, [btnArr]);
 
   return (
     <Div>
       <SimpleNavBar text="단어장" />
 
       <Container>
-        {savedWordList.map((w) => (
-          <div
-            key={w.word_id}
-            style={style}
-            onClick={() => _handleWordMean(w.word_id)}
-          >
-            <p>
-              {w.word_id} {w.word} {w.meaning} {w.word_date}
-            </p>
+        <Banner>
+          <div>
+            <Title>
+              내가 저장한 <br /> 믿어방 부동산 단어
+            </Title>
+
+            <Buttons>
+              {btnArr.map((b) => (
+                <Btn active={b.active} onClick={() => _handleClickBtn(b.id)}>
+                  {b.text}
+                </Btn>
+              ))}
+            </Buttons>
           </div>
+
+          <img src={wordbook} width={160} height={137} />
+        </Banner>
+
+        <CountText>
+          저장된 단어 <span>{savedWordList.length}개</span>
+        </CountText>
+        {savedWordList.map((w) => (
+          <WordBox key={w.wordId} onClick={() => _handleWordMean(w.wordId)}>
+            <div className="top-content">
+              <p>{w.word}</p>
+              <span>{w.word_date}</span>
+              {/* {w.word_date.split("T")[0]} */}
+            </div>
+
+            <p className="mean">{w.meaning}</p>
+
+            <img src={bookmark} width={16} height={23} />
+          </WordBox>
         ))}
       </Container>
     </Div>
@@ -83,93 +141,126 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  padding: 30px 15px;
 `;
 
-const Illustration = styled.img`
-  width: 179px;
-  height: 115px;
-  margin: 15px auto 0 auto;
-`;
-
-const Calendar = styled.img`
-  width: 39px;
-  height: 39px;
-  margin-left: auto;
-  margin-right: 22px;
-`;
-
-const BtnBox = styled.div`
+const Banner = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 0 23px;
-  margin-top: 19px;
-  margin-bottom: 37px;
+
+  width: 100%;
+
+  margin-bottom: 35px;
 `;
-const Button = styled.div<{ active: boolean }>`
-  width: 50%;
-  height: 45px;
-  border-radius: 4px;
+
+const Title = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 32px;
+
+  color: #000000;
+
+  margin-bottom: 29px;
+  margin-left: 5px;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  align-content: center;
+`;
+
+const Btn = styled.div<{ active: boolean }>`
+  margin-right: 10px;
+  width: 107px;
+  height: 29px;
+  border-radius: 9px;
+
   display: flex;
   justify-content: center;
   align-items: center;
 
   font-family: "Noto Sans KR";
   font-style: normal;
-  font-weight: 400;
-  font-size: 17px;
-  line-height: 25px;
+  font-weight: 300;
+  font-size: 12px;
+  text-align: center;
 
-  border: 1px solid ${(props) => (props.active ? "#1F4EF5" : "#9a9a9a")};
-  color: ${(props) => (props.active ? "#1F4EF5" : "#9a9a9a")};
+  color: ${(props) => (props.active ? "#ffffff" : "#707070")};
+  background: ${(props) => (props.active ? "#5a73fc" : "#F2F3F7")};
 
-  margin-left: 7px;
-  margin-right: 7px;
+  ${(props) =>
+    props.active &&
+    css`
+      box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+    `};
 `;
 
-const Word = styled.div`
-  width: 331px;
-  height: auto;
+const CountText = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23px;
+  color: #313131;
+
+  span {
+    color: #7b7b7b;
+    margin-left: 7px;
+  }
+
+  margin-bottom: 16px;
+`;
+
+const WordBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 156px;
   background: #f4f5f7;
   border-radius: 5px;
 
-  margin: 16px auto 0 auto;
-  padding: 20px;
+  padding: 23px 18px;
+  margin-bottom: 13px;
 
-  .title {
-    display: flex;
-    justify-content: space-between;
+  &:active {
+    background: #eceef3;
   }
 
-  .word {
+  .top-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
     font-family: "Noto Sans KR";
     font-style: normal;
     font-weight: 400;
     font-size: 17px;
-    line-height: 25px;
+
+    span {
+      font-weight: 350;
+      font-size: 14px;
+
+      color: rgba(0, 0, 0, 0.5);
+    }
+
+    margin-bottom: 10px;
   }
 
-  .date {
-    font-family: "Noto Sans KR";
-    font-style: normal;
-    font-weight: 350;
-    font-size: 14px;
-    line-height: 20px;
-
-    color: rgba(0, 0, 0, 0.5);
-  }
-
-  .des {
-    margin-top: 10px;
-    margin-bottom: 15px;
-
+  .mean {
     font-family: "Noto Sans KR";
     font-style: normal;
     font-weight: 300;
     font-size: 14px;
     line-height: 20px;
+
+    color: #000000;
   }
 
-  .bookmark {
-    z-index: 1000;
+  img {
+    margin-left: auto;
   }
 `;
