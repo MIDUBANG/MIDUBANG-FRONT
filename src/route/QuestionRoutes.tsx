2cb409770aaa8a7f2c1851ff76fe51926ce23337
dy657 <dy6578ekdbs@gmail.com>
2023-04-27 23:12:34 +0900

    ✨ 챗/금쪽이 라우트 분리

--- src/route/QuestionRoutes.tsx
+++ src/route/QuestionRoutes.tsx
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom";
 
 import QuestionMain from "@pages/Question/Main";
 import QuestionList from "@pages/Question/List";
-import QuestionDetail from "@pages/Question/Detail";
+import QuestionGoldDetail from "@pages/Question/Detail/GoldDetail";
+import QuestionChatDetail from "@pages/Question/Detail/ChatDetail";
+
 import QuestionCreate from "@pages/Question/Create";
 
 const QuestionRoutes = () => {
@@ -11,7 +13,10 @@ const QuestionRoutes = () => {
     <Routes>
       <Route element={<QuestionMain />} path="/" />
       <Route element={<QuestionList />} path="/list" />
-      <Route element={<QuestionDetail />} path="/detail/:id" />
+
+      <Route element={<QuestionGoldDetail />} path="/gold/:id" />
+      <Route element={<QuestionChatDetail />} path="/chat/:id" />
+
       <Route element={<QuestionCreate />} path="/create" />
     </Routes>
   );
