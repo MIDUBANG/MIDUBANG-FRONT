ca37ccbcb0135273148328ebd531b9d81dbfcbaa
dy657 <dy6578ekdbs@gmail.com>
2023-04-28 16:53:31 +0900

    ✨my페이지 > 분석 기록

--- src/pages/User/Analyze/AnalyzeHistory.tsx
+++ src/pages/User/Analyze/AnalyzeHistory.tsx
@@ -1,63 +1,58 @@
 // hooks
 import { useCookies } from "react-cookie";
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import styled from "@emotion/styled";
+import { useNavigate, useParams } from "react-router-dom";
 // component
 import SimpleNavBar from "@components/NavBar/SimpleNavBar";
 import ResultBox from "@components/Result/ResultBox";
 import WordModal from "@components/Modal/WordModal";
 //api
 import { GetAnalyze, DeleteAnalyze } from "@api/analyze";
-
+import { RootState } from "@store/store";
+import { useAppSelector, useAppDispatch } from "@store/store";
+import { setReportId } from "@store/summarySlice";
 // asset
 import { resultsType, CasesType, WordsType } from "@assets/types";
 import temp from "@assets/result/temp.png";
 import CommissionBox from "@components/Result/CommissionBox";
 
-const Result = () => {
-  const { id } = useParams() as { id: string };
-  const record_id = parseInt(id);
-
-  const navigate = useNavigate();
-
+const AnalyzeHistory = () => {
   const [results, setResults] = useState<resultsType>();
   const [cases, setCases] = useState<CasesType[]>([]);
   const [words, setWords] = useState<WordsType[]>([]);
   const [contractType, setContractType] = useState<string>("");
 
-  /** 개별 분석을 불러오는 함수 */
-  const _handlePostAnalyze = async () => {
-    const res = await GetAnalyze(record_id, cookies.refreshToken, onCookie);
+  let { id } = useParams();
 
-    setResults(res.record);
-    setCases(res.myCaseDto);
-    setWords(res.simpleWordDtos);
+  /** 레포트 기록 하나 가져오기 */
+  const _handleGetAnalyze = async () => {
+    const reportId = parseInt(id || "0");
 
-    if (res.record.contract_type === "MONTHLY_RENT") {
+    const analyzeResult = await GetAnalyze(
+      reportId,
+      cookies.refreshToken,
+      onCookie
+    );
+
+    console.log("요청 결과 >>>>>>>>", analyzeResult);
+
+    const resultRecord = analyzeResult.record;
+    const resultCase = analyzeResult.myCaseDto;
+    const resultWord = analyzeResult.simpleWordDtos;
+
+    setResults(resultRecord);
+    setCases(resultCase);
+    setWords(resultWord);
+
+    if (resultRecord.contract_type === "MONTHLY_RENT") {
       setContractType("월세");
-    } else if (res.record.contract_type === "JEONSE") {
+    } else if (resultRecord.contract_type === "JEONSE") {
       setContractType("전세");
     } else {
       setContractType("반전세");
     }
   };
-
-  /** 분석 기록을 삭제하는 함수 */
-  const _handleDeleteAnalyze = async () => {
-    const res = await DeleteAnalyze(record_id, cookies.refreshToken, onCookie);
-    navigate(-1);
-  };
-
-  /** 분석 삭제를 확인하는 모달 오픈하는 함수 */
-  const _handleOpenDeleteModal = () => {
-    alert("분석을 삭제하시겠습니까?");
-    _handleDeleteAnalyze();
-  };
-
-  useEffect(() => {
-    _handlePostAnalyze();
-  }, []);
 
   const [cookies, setCookie, removeCookie] = useCookies(["refreshToken"]);
   const onCookie = (res: any) => {
@@ -79,74 +74,91 @@ const Result = () => {
     setModalOpen(false);
   };
 
+  const [customDirection, setCustomDirecton] = useState("up");
+
+  const navigate = useNavigate();
+  // const _clickSummaryBtn = async () => {
+  //   dispatch(
+  //     setReportId({
+  //       reportId: results?.recordId,
+  //     })
+  //   );
+  //   navigate("/summary");
+  // };
+
+  useEffect(() => {
+    console.log("useEffect 실행");
+    _handleGetAnalyze();
+  }, []);
+
+  const _handleClickDelete = async () => {
+    const reportId = parseInt(id || "0");
+
+    await DeleteAnalyze(reportId, cookies.refreshToken, onCookie);
+
+    navigate("/my/analyzelist");
+  };
+
   return (
     <Div>
-      <SimpleNavBar text="레포트" />
-      <Container>
-        <Title>
-          <div></div>
-          <p> 주의 요망 특약 사항 발견</p>
-        </Title>
-
-        <Date>2023.02.20 분석</Date>
-
-        <ImgBox>
-          <ContractImg src={temp} />
-          <div>{contractType}</div>
-        </ImgBox>
-
-        <CommissionBox
-          answer_commission={results?.answer_commission}
-          my_commission={results?.answer_commission}
-          is_expensive={results?.is_expensive}
-        />
-
-        {cases.map((c) => {
-          return (
-            <ResultBox
-              key={c.case_id}
-              caseData={c}
-              wordData={words}
-              openWordModal={openWordModal}
-            />
-          );
-        })}
-
+      {modalOpen && (
         <WordModal
           open={modalOpen}
           close={closeWordModal}
           text={selectWord}
           wordsData={words}
         />
+      )}
+
+      <Container>
+        <SimpleNavBar text="레포트 결과" />
+        <Title>
+          <div></div>
+          <p> 특약 조항 분석 레포트</p>
+          <button onClick={_handleClickDelete}>삭제</button>
+        </Title>
+        {/* <Date>{results?.record_date.replaceAll("-", ". ")} 분석</Date> */}
+        <Date>{results?.record_date} 분석</Date>
+
+        <ImgBox>
+          <ContractImg src={temp} />
+          <div>{contractType}</div>
+        </ImgBox>
+
+        {/* <div onClick={_clickSummaryBtn}>레포트 요약 보기</div> */}
+
+        <CommissionBox
+          answer_commission={results?.answer_commission}
+          my_commission={results?.commission}
+          is_expensive={results?.is_expensive}
+        />
+        {cases.map((c) => {
+          return (
+            <ResultBox
+              key={c.caseId}
+              caseData={c}
+              wordData={words}
+              openWordModal={openWordModal}
+            />
+          );
+        })}
       </Container>
-      <BottomNav>
-        <p onClick={_handleOpenDeleteModal}>삭제</p>
-      </BottomNav>
     </Div>
   );
 };
 
-export default Result;
-
-const BottomNav = styled.div`
-  border-top: 2px solid #f4f5f7;
-  width: 100%;
-  height: 60px;
-
-  position: fixed;
-  bottom: 0;
-
-  background-color: white;
-`;
+export default AnalyzeHistory;
 
 const Div = styled.div`
   width: 100%;
   height: 100%;
-
-  overflow-y: scroll;
+  padding-top: 90px;
+  //overflow-y: scroll;
 `;
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   padding-bottom: 100px;
 `;
 
--- src/pages/User/Analyze/AnalyzeList.tsx
+++ src/pages/User/Analyze/AnalyzeList.tsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
 import { GetAnalyzeList, GetAnalyze, DeleteAnalyze } from "@api/analyze";
 import { useCookies } from "react-cookie";
+// img
+import report from "@assets/analyzelist/report.png";
 // asset
 import SimpleNavBar from "@components/NavBar/SimpleNavBar";
 import { resultsType } from "@assets/types";
 
-const AnalyzeList = () => {
+const AnalyzeHistory = () => {
   const [results, setResults] = useState<resultsType[]>([]);
 
   const navigate = useNavigate();
@@ -15,8 +19,7 @@ const AnalyzeList = () => {
   /** 분석 기록 리스트 */
   const _handleGetAnalyzeList = async () => {
     const res = await GetAnalyzeList(cookies.refreshToken, onCookie);
-    setResults(res.recordList);
-    console.log("분석 리스트", res);
+    setResults(res.data.recordList);
   };
 
   const [cookies, setCookie, removeCookie] = useCookies(["refreshToken"]);
@@ -30,16 +33,46 @@ const AnalyzeList = () => {
 
   /**분석 개별 페이지로 이동 */
   const _handleClickAnalyze = (id: number) => {
-    navigate(`/analyze/${id}`);
+    navigate(`${id}`);
   };
 
-  // answer_commission: 0;
-  // commission: 0;
-  // contract_type: "JEONSE";
-  // image_url: "https://tempimgurl";
-  // is_expensive: false;
-  // record_date: "2023-02-20";
-  // record_id: 308;
+  const _handleClickBtn = (id: number) => {
+    setBtnArr(
+      btnArr.map((b) =>
+        b.id === id ? { ...b, active: true } : { ...b, active: false }
+      )
+    );
+  };
+
+  const [btnArr, setBtnArr] = useState([
+    { id: 1, text: "최신순", active: true },
+    { id: 2, text: "과거순", active: false },
+  ]);
+
+  /** 정렬 */
+  useEffect(() => {
+    console.log("정렬결과");
+
+    var newArray = [...results];
+
+    if (btnArr[0].active) {
+      newArray.sort((a, b) => {
+        // 최신순
+        if (a.record_date > b.record_date) return -1;
+        if (a.record_date < b.record_date) return 1;
+        return 0;
+      });
+    } else {
+      newArray.sort((a, b) => {
+        // 과거순
+        if (a.record_date > b.record_date) return 1;
+        if (a.record_date < b.record_date) return -1;
+        return 0;
+      });
+    }
+
+    setResults(newArray);
+  }, [btnArr]);
 
   useEffect(() => {
     _handleGetAnalyzeList();
@@ -47,10 +80,31 @@ const AnalyzeList = () => {
 
   return (
     <Div>
-      <SimpleNavBar text="분석 목록" />
+      <SimpleNavBar text="레포트 목록" />
 
       <Container>
-        {results.map((result) => {
+        <Banner>
+          <div>
+            <Title>
+              내가 저장한 <br /> 믿어방 부동산 단어
+            </Title>
+
+            <Buttons>
+              {btnArr.map((b) => (
+                <Btn active={b.active} onClick={() => _handleClickBtn(b.id)}>
+                  {b.text}
+                </Btn>
+              ))}
+            </Buttons>
+          </div>
+
+          <img src={report} width={160} height={137} />
+        </Banner>
+
+        <CountText>
+          저장된 레포트 <span>{results.length}개</span>
+        </CountText>
+        {results?.map((result) => {
           let date =
             result.record_date.substring(5, 10).replace("-", "월 ") + "일";
           let contractType = "";
@@ -63,9 +117,9 @@ const AnalyzeList = () => {
           }
 
           return (
-            <AnalyzeBox onClick={() => _handleClickAnalyze(result.record_id)}>
-              <p className="title">{contractType} 분석</p>
-              <p className="date">{date}</p>
+            <AnalyzeBox onClick={() => _handleClickAnalyze(result.recordId)}>
+              <p className="title">{date} 분석</p>
+              <p className="date">{contractType} </p>
             </AnalyzeBox>
           );
         })}
@@ -73,17 +127,146 @@ const AnalyzeList = () => {
     </Div>
   );
 };
-export default AnalyzeList;
+export default AnalyzeHistory;
 
 const Div = styled.div`
   width: 100%;
   height: 100%;
+
+  padding-top: 70px;
 `;
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  padding: 30px 15px;
+`;
+
+const Banner = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+
+  margin-bottom: 35px;
+
+  img {
+    width: 140px;
+    height: auto;
+  }
+`;
+
+const Title = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 32px;
+
+  color: #000000;
+
+  margin-bottom: 29px;
+  margin-left: 5px;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  align-content: center;
+`;
+
+const Btn = styled.div<{ active: boolean }>`
+  margin-right: 10px;
+  width: 107px;
+  height: 29px;
+  border-radius: 9px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  text-align: center;
+
+  color: ${(props) => (props.active ? "#ffffff" : "#707070")};
+  background: ${(props) => (props.active ? "#5a73fc" : "#F2F3F7")};
+
+  ${(props) =>
+    props.active &&
+    css`
+      box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+    `};
+`;
+
+const CountText = styled.p`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23px;
+  color: #313131;
+
+  span {
+    color: #7b7b7b;
+    margin-left: 7px;
+  }
+
+  margin-bottom: 16px;
+`;
+
+const WordBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 156px;
+  background: #f4f5f7;
+  border-radius: 5px;
+
+  padding: 23px 18px;
+  margin-bottom: 13px;
+
+  &:active {
+    background: #eceef3;
+  }
+
+  .top-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+
+    span {
+      font-weight: 350;
+      font-size: 14px;
+
+      color: rgba(0, 0, 0, 0.5);
+    }
+
+    margin-bottom: 10px;
+  }
+
+  .mean {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 20px;
+
+    color: #000000;
+  }
+
+  img {
+    margin-left: auto;
+  }
 `;
 
 const AnalyzeBox = styled.div`
@@ -91,12 +274,12 @@ const AnalyzeBox = styled.div`
   justify-content: space-between;
   align-items: center;
 
-  width: 331px;
+  width: 100%;
   height: auto;
   background: #f4f5f7;
   border-radius: 5px;
 
-  margin: 10px auto 0 auto;
+  margin-bottom: 15px;
   padding: 20px;
 
   .title {
