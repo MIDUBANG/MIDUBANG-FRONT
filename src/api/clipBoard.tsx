bae6299c555198bd35278a523b20bd2fcd698eeb
dy657 <dy6578ekdbs@gmail.com>
2023-05-11 19:04:11 +0900

    🐛클립보드 복사 alert 수정

--- src/api/clipBoard.tsx
+++ src/api/clipBoard.tsx
@@ -1,7 +1,7 @@
 export const handleCopyClipBoard = async (text: string) => {
   try {
     await navigator.clipboard.writeText(text);
-    alert("클립보드에 링크가 복사되었습니다.");
+    alert("클립보드에 메세지가 복사되었습니다.");
   } catch (e) {
     alert("복사에 실패하였습니다");
   }
