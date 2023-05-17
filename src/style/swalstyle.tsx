61e4f2acc27b4b2f5c8928dea88a58433b1985c2
dy657 <dy6578ekdbs@gmail.com>
2023-05-17 15:09:20 +0900

    🐛 Swal 버튼 순서 reverse

--- src/style/swalstyle.tsx
+++ src/style/swalstyle.tsx
@@ -1,15 +1,3 @@
-export const loginStyle = {
-  title: "접근 실패",
-  text: "로그인이 필요한 페이지입니다",
-  icon: "warning",
-  iconColor: "green",
-  showConfirmButton: false,
-  timer: 2000,
-  background: "white",
-  color: "black",
-  backdrop: "rgba(0,0,0,0.2)",
-};
-
 export const logoutStyle = {
   title: "로그아웃 할까요?",
   text: "다시 되돌릴 수 없습니다.",
@@ -30,38 +18,6 @@ export const logoutStyle = {
   backdrop: "rgba(0,0,0,0.2)",
 
   closeOnClickOutside: false,
+
+  reverseButtons: true,
 };
-
-export const notValidEmailStyle = {
-  title: "로그인 실패",
-  text: "이메일의 형식이 유효하지 않습니다.",
-  icon: "warning",
-  iconColor: "green",
-  showConfirmButton: false,
-  timer: 2000,
-  background: "white",
-  color: "black",
-  backdrop: "rgba(0,0,0,0.2)",
-};
-
-// export const SwalError = {
-//   width: 400,
-//   background: palette.black,
-//   color: palette.white,
-//   text: "에러가 발생했습니다.",
-//   icon: "error",
-//   showConfirmButton: false,
-//   timer: 1500,
-//   backdrop: palette.backdrop,
-// };
-
-// export const SwalEmpty = {
-//   text: "입력하지 않은 내용이 있습니다.",
-//   icon: "warning",
-//   iconColor: palette.green,
-//   showConfirmButton: false,
-//   timer: 1000,
-//   background: palette.black,
-//   color: palette.white,
-//   backdrop: palette.backdrop,
-// };
