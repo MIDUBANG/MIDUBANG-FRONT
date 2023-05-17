854c7c1aba18ac8753e96f7d7004a530b485875d
dy657 <dy6578ekdbs@gmail.com>
2023-05-17 16:18:44 +0900

    💄 QuickMenu animation 구현

---
+++ src/components/Word/QuickBookMark.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import scrollbookmark from "@assets/wordlist/scrollbookmark.png";
+
+const QuickBookMark = () => {
+  const markRef = useRef<HTMLDivElement>(null);
+
+  const navigate = useNavigate();
+
+  const _handleGotoMyBookmark = () => {
+    navigate("/my/savedword");
+  };
+
+  const _handleUpdatePosition = () => {
+    if (markRef.current) {
+      const regex = /[^0-9]/g;
+      let currentPosition: any = markRef.current.style.top.replace(regex, "");
+      currentPosition = parseInt(currentPosition);
+      console.log(currentPosition);
+      let position = window.scrollY;
+
+      //markRef.current.animate({ top: position + 200 + "px" }, 1000);
+
+      markRef.current.style.top = position + 200 + "px";
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", _handleUpdatePosition); // add event listener
+
+    return () => {
+      window.removeEventListener("scroll", _handleUpdatePosition); // clean up
+    };
+  }, []);
+
+  return (
+    <ScrollBookMark
+      onClick={_handleGotoMyBookmark}
+      ref={markRef}
+      style={{ top: "200px" }}
+    >
+      <img src={scrollbookmark} width={61} height={35} />
+      <p>단어장</p>
+    </ScrollBookMark>
+  );
+};
+
+export default QuickBookMark;
+
+const ScrollBookMark = styled.div`
+  transition: all 800ms ease 100ms;
+  z-index: 100;
+  position: absolute;
+  right: 0;
+  display: flex;
+  align-items: center;
+  p {
+    position: absolute;
+    left: 50%;
+
+    width: 40px;
+    transform: translate(-30%);
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    color: #ffffff;
+  }
+`;
