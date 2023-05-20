b1e08bad38b92c61d981a53dfceb6f327087ec47
dy657 <dy6578ekdbs@gmail.com>
2023-05-20 15:59:27 +0900

    💄upload first - 반응형 이슈

--- src/pages/Analyze/Slide/First.tsx
+++ src/pages/Analyze/Slide/First.tsx
@@ -5,13 +5,13 @@ const First = () => {
   return (
     <Div>
       <Illustration src={illustration} />
-      <Title margin="67px 0 0 0">임대차계약서 분석 서비스 </Title>
-      <SmallText margin="33px 0 0 0">
+      <Title margin="10vh 0 0 0">임대차계약서 분석 서비스 </Title>
+      <SmallText margin="5vh 0 0 0">
         처음 하는 부동산 계약, <br /> 불리한 계약이 아닌지 불안하다면?
       </SmallText>
 
-      <SmallText margin="33px 0 0 0">
-        믿어방 AI 스캐너를 이용해 <br /> 임대차계약서를 분석해보세요.{" "}
+      <SmallText margin="5vh 0 0 0">
+        믿어방 AI 스캐너를 이용해 <br /> 임대차계약서를 분석해보세요.
       </SmallText>
     </Div>
   );
@@ -26,7 +26,7 @@ const Div = styled.div`
 `;
 
 const Illustration = styled.img`
-  width: 198px;
+  width: 30vh;
   margin: 35px auto 0 auto;
   transform: translate(-11px, 0);
 `;
@@ -41,7 +41,7 @@ const Title = styled.p<{ margin: string }>`
 
   text-align: center;
 
-  margin: ${(props) => props.margin};
+  margin: ${props => props.margin};
 `;
 
 const SmallText = styled.p<{ margin: string }>`
@@ -52,30 +52,5 @@ const SmallText = styled.p<{ margin: string }>`
   line-height: 23px;
   text-align: center;
   color: #7d7d7d;
-  margin: ${(props) => props.margin};
-`;
-
-const IconDiv = styled.div`
-  margin: 0 50px;
-  margin-top: 30px;
-  display: flex;
-  justify-content: space-around;
-`;
-
-const Icon = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  p {
-    margin-top: 8px;
-
-    text-align: center;
-
-    font-family: "Noto Sans KR", sans-serif;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 17px;
-  }
+  margin: ${props => props.margin};
 `;
