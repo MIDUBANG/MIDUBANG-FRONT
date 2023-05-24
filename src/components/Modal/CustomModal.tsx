158d43b9588c777095203913f57ed0f8f7675095
dy657 <dy6578ekdbs@gmail.com>
2023-05-24 16:23:43 +0900

    ✨Swalmodal timer 인수 추가

--- src/components/Modal/CustomModal.tsx
+++ src/components/Modal/CustomModal.tsx
@@ -1,39 +1,39 @@
 import Swal from "sweetalert2";
 
-export const ErrorSwal = (title: string, text: string) =>
+export const ErrorSwal = (title: string, text: string, time: number = 2000) =>
   Swal.fire({
     title: title,
     text: text,
     icon: "error",
     iconColor: "red",
     showConfirmButton: false,
-    timer: 2000,
+    timer: time,
     background: "white",
     color: "black",
     backdrop: "rgba(0,0,0,0.2)",
   });
 
-export const WarningSwal = (title: string, text: string) =>
+export const WarningSwal = (title: string, text: string, time: number = 2000) =>
   Swal.fire({
     title: title,
     text: text,
     icon: "warning",
     iconColor: "orange",
     showConfirmButton: false,
-    timer: 2000,
+    timer: time,
     background: "white",
     color: "black",
     backdrop: "rgba(0,0,0,0.2)",
   });
 
-export const SuccessSwal = (title: string, text: string) =>
+export const SuccessSwal = (title: string, text: string, time: number = 2000) =>
   Swal.fire({
     title: title,
     text: text,
     icon: "success",
     iconColor: "green",
     showConfirmButton: false,
-    timer: 2000,
+    timer: time,
     background: "white",
     color: "black",
     backdrop: "rgba(0,0,0,0.2)",
